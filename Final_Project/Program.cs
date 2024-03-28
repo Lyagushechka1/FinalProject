0e849db39aae85651ad5628662543face1248ae9
Lyagushka <lp021378@gmail.com>
2024-03-28 21:00:42 +0200

    Add the BETA app of my project

---
+++ Final_Project/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+/*
+Щоденник заміток:
+Створіть клас Diary для представлення кожної замітки з властивостями, такими як заголовок, текст, дата створення тощо.
+Реалізуйте клас NoteManager, який буде відповідати за додавання, видалення та редагування записів у щоденнику.
+Використовуйте серіалізацію XML для зберігання та завантаження списку заміток.
+*/
+namespace Final_project
+{
+    public class Diary
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public DateTime DateOfReg { get; set; }
+    }
+    public class NoteManager
+    {
+        public void Add(string xmlFilePath)
+        {
+        }
+        //TODO: public void Remove() { }
+        //TODO: public void Edit() { }
+    }
+    public class Project
+    {
+        public void Main()
+        {
+            Diary diary = new Diary
+            {
+                Title = "Заголовок",
+                Text = "Сделать дз",
+                DateOfReg = new DateTime(2006, 04, 25),
+            };
+        }
+    }
+}
